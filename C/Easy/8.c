0bc7a74f783aa1a7b46e1e27f88a2de9b1e1df20
Snipe3t <sanjitkore1827@gmail.com>
2024-10-05 13:08:45 +0530

    Update 8.c
    404notfound

--- C/Easy/8.c
+++ C/Easy/8.c
@@ -9,6 +9,6 @@ int main() {
     struct peron p1;
     p1.age = 25;
     p1.height = 5.9;
-    printf("Age: %d, Height: %.1f", p1.age, p1.height);
+    printf("Age: %d, Height: %f", p1.age, p1.height);
     return 0;
 }
