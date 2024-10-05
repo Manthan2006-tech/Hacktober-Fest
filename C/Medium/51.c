837990cf4a5016711841a0b564936b6e488c82d6
Snipe3t <sanjitkore1827@gmail.com>
2024-10-05 13:57:37 +0530

    404notfound
    404notfound

--- C/Medium/51.c
+++ C/Medium/51.c
@@ -1,10 +1,12 @@
 
 #include <stdio.h>
-void myFunction(int a);
+
+void myFunction(int a) {
+    printf("%d\n", a);
+}
+
 int main() {
     myFunction(5.5);
     return 0;
 }
-void myFunction(int a) {
-    printf("%d\n", a);
-}
+
