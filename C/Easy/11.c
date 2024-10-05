55f00ed05986db17ec6809a51b18b98e422782fe
Snipe3t <sanjitkore1827@gmail.com>
2024-10-05 13:15:08 +0530

    Update 11.c
    404notfound

--- C/Easy/11.c
+++ C/Easy/11.c
@@ -3,8 +3,8 @@
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(int);
-    for (int i = 0; i <= size; i++) {
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
