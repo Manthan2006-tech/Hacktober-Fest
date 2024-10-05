da64dce135eba05496069cb2ce3a52ca1dd1c2c0
Manthan2006-tech <manthanc714@gmail.com>
2024-10-05 16:00:16 +0530

    Update 59.py
    aarya 404notfound

--- Python/Medium/59.py
+++ Python/Medium/59.py
@@ -1,15 +1,16 @@
 def factorial(n):
- if n < 0:
- return "Factorial not defined for negative numbers"
- result = 1
- for i in range(n):
- result *= i
- return result
- def main():
- try:
- value = 5
- result = factorial(value)
- print(f"Factorial of {value} is {result}")
- except Exception as e:
- print(f"Error: {e}")
- main()
+   if n < 0:
+     return "Factorial not defined for negative numbers"
+   result = 1
+   for i in range(result,n+1,1):
+     result *= i
+   return result
+def main():
+    try:
+       value = 5
+       result = factorial(value)
+       print(f"Factorial of {value} is {result}")
+    except Exception as e:
+       print(f"Error: {e}")
+
+main()
